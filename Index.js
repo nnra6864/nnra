df58ecb68cbea5acd777d7067589e391d87540ef
nnra6864 <enenragd@gmail.com>
2024-02-01 22:08:36 +0100

    Pre Hypr

--- Index.js
+++ Index.js
@@ -1,42 +1,41 @@
 var hasLoaded = false;
 var pages;
+var isSwitching = false;
 
 async function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-document.addEventListener('DOMContentLoaded', loadPage)
+document.addEventListener('DOMContentLoaded', loadPageNew)
 
-async function loadPage(){
+async function loadPageNew(){
     pages = document.getElementById('Pages');
     await delay(50);
+    var loadingContainer = document.getElementById('LoadingContainer');
+    loadingContainer.classList.add('Load');
+
+    await delay(3000);
+    var content = document.getElementById('Content');
+    content.classList.add('ShowPages');
+    await delay(175);
     var header = document.getElementById('Header');
     header.classList.add('Load');
-
-    await delay(750);
-    var loadingContainer = document.getElementById('LoadingContainer');
-    loadingContainer.classList.add('Load')
-
-    await delay(250)
-    //Has to be a total of 1000ms before this
-    var headerButtons = document.getElementsByClassName('HeaderButton');
-    Array.from(headerButtons).forEach(element => {element.classList.add('Loaded')});
-
     var pageContainers = document.getElementsByClassName('PageContainer');
     Array.from(pageContainers).forEach(element => {element.classList.add('Load')});
 
-    await delay(3000)
-    var content = document.getElementById('Content');
-    content.classList.add('ShowPages');
-
-    await delay(1000)
-
+    //Has to be a total of 1000ms before this
+    await delay(1000);
+    loadingContainer.classList.add('Unload');
+    var headerButtons = document.getElementsByClassName('HeaderButton');
+    Array.from(headerButtons).forEach(element => {element.classList.add('Loaded')});
+    
     hasLoaded = true;
 }
 
-function displayPage(i){
-    console.log('here');
-    if (!hasLoaded) return;
+async function displayPage(i){
+    if (!hasLoaded || isSwitching) return;
+    isSwitching = true;
     pages.style.transform = `translateX(${i === 0 ? 100 : i === 1 ? 0 : -100}vw)`;
-    console.log('displayed');
+    await delay(500);
+    isSwitching = false;
 }
