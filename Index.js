d6ee0598e64fe49d0f624e80e84de3465ad699f4
nnra6864 <enenragd@gmail.com>
2024-02-02 23:17:50 +0100

    Mostly finished the image grid, gotta add elasticity on the bottom

--- Index.js
+++ Index.js
@@ -1,6 +1,9 @@
-var hasLoaded = false;
-var pages;
-var isSwitching = false;
+let hasLoaded = false;
+let pages = document.getElementById('Pages');
+let isSwitching = false;
+let loadedProjects = false, loadedAssets = false;
+let overlay = document.getElementById("Overlay");
+let overlayTransitioning = false;
 
 async function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -9,24 +12,23 @@ async function delay(ms) {
 document.addEventListener('DOMContentLoaded', loadPageNew)
 
 async function loadPageNew(){
-    pages = document.getElementById('Pages');
     await delay(50);
-    var loadingContainer = document.getElementById('LoadingContainer');
+    let loadingContainer = document.getElementById('LoadingContainer');
     loadingContainer.classList.add('Load');
 
     await delay(3000);
-    var content = document.getElementById('Content');
+    let content = document.getElementById('Content');
     content.classList.add('ShowPages');
     await delay(175);
-    var header = document.getElementById('Header');
+    let header = document.getElementById('Header');
     header.classList.add('Load');
-    var pageContainers = document.getElementsByClassName('PageContainer');
+    let pageContainers = document.getElementsByClassName('PageContainer');
     Array.from(pageContainers).forEach(element => {element.classList.add('Load')});
 
     //Has to be a total of 1000ms before this
     await delay(1000);
     loadingContainer.classList.add('Unload');
-    var headerButtons = document.getElementsByClassName('HeaderButton');
+    let headerButtons = document.getElementsByClassName('HeaderButton');
     Array.from(headerButtons).forEach(element => {element.classList.add('Loaded')});
     
     hasLoaded = true;
@@ -38,4 +40,26 @@ async function displayPage(i){
     pages.style.transform = `translateX(${i === 0 ? 100 : i === 1 ? 0 : -100}vw)`;
     await delay(500);
     isSwitching = false;
+    let images = [];
+    if (i === 0 && !loadedProjects) { images = document.getElementsByClassName("ProjectImage"); loadedProjects = true; }
+    else if (i === 2 && !loadedAssets) { images = document.getElementsByClassName("AssetImage"); loadedAssets = true; }
+    for (const image of images) {
+        loadGridImage(image);
+        await delay(100);
+    }
+}
+
+async function loadGridImage(image){
+    image.classList.add('Load');
+    await delay(1000)
+    image.classList.add('Loaded');
+}
+
+async function toggleOverlay(shown){
+    if (overlayTransitioning) return;
+    overlayTransitioning = true;
+    if (shown) overlay.classList.add('Shown');
+    else overlay.classList.remove('Shown');
+    await delay(500);
+    overlayTransitioning = false;
 }
